0db9c13c86d5337c7f664e19c42c999d08d6ed34
architch <architagarwal123@gmail.com>
2025-10-28 22:53:55 +0530

    address review : simplify

--- driver/src/main/scala/com/datastax/spark/connector/types/TypeConverter.scala
+++ driver/src/main/scala/com/datastax/spark/connector/types/TypeConverter.scala
@@ -274,13 +274,13 @@ object TypeConverter {
     '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'
   )
   private def byteArrayToString (x: Array[Byte]) : String = {
-    val result = new StringBuilder(x.length * 2) 
+    val result = new StringBuilder(x.length * 2 + 2).append("0x")
     x.foreach { b =>
       val v = b & 0xFF
       result.append(HEX_CHARS(v >>> 4))
       result.append(HEX_CHARS(v & 0x0F))
     }
-    "0x" + result.toString()
+    result.toString()
   }
   private def stringToByteArray (x: String)  = new BigInteger(x.substring(2), 16).toByteArray
 
